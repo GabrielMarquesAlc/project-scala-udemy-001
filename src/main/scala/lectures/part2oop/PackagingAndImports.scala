7c056bc0a2844bd53519d7dde163f01e950a78dd
GabrielMarquesAlc <gabriel.alcantara@dpsp.com.br>
2022-01-23 16:06:29 -0300

    new package

--- src/main/scala/lectures/part2oop/PackagingAndImports.scala
+++ src/main/scala/lectures/part2oop/PackagingAndImports.scala
@@ -1,7 +1,15 @@
 package lectures.part2oop
 
+import playground._//{Cinderella, PrinceCharming}
+
 object PackagingAndImports extends App{
 
   val writer = new Writer("Gabriel","Gabriel Marques",2022);
+  val cinderela = new Cinderella
 
+  sayHello
+  println(SPEED_OF_LIGHT)
+
+  //imports
+  val prince = new PrinceCharming
 }
