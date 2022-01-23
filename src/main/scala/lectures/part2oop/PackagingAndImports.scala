a920260446835d9777d8f2ff74ae70625b13c27f
GabrielMarquesAlc <gabriel.alcantara@dpsp.com.br>
2022-01-23 16:08:09 -0300

    import package java

--- src/main/scala/lectures/part2oop/PackagingAndImports.scala
+++ src/main/scala/lectures/part2oop/PackagingAndImports.scala
@@ -1,15 +1,36 @@
 package lectures.part2oop
 
-import playground._//{Cinderella, PrinceCharming}
+import playground.{PrinceCharming, Cinderella => Princess}
 
-object PackagingAndImports extends App{
+import java.util.Date
+import java.sql.{Date => SqlDate}
 
-  val writer = new Writer("Gabriel","Gabriel Marques",2022);
-  val cinderela = new Cinderella
+object PackagingAndImports extends App {
 
+  // package members are accessible by their simple name
+  val writer = new Writer("Daniel", "RockTheJVM", 2018)
+
+  // import the package
+  val princess = new Princess  // playground.Cinderella = fully qualified name
+
+  // packages are in hierarchy
+  // matching folder structure.
+
+  // package object
   sayHello
   println(SPEED_OF_LIGHT)
 
-  //imports
+  // imports
   val prince = new PrinceCharming
+
+  // 1. use FQ names
+  val date = new Date
+  val sqlDate = new SqlDate(2018, 5, 4)
+  // 2. use aliasing
+
+  // default imports
+  // java.lang - String, Object, Exception
+  // scala - Int, Nothing, Function
+  // scala.Predef - println, ???
+
 }
